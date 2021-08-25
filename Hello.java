555323efae5e915e59dacd06daea9742836b4486
penumalays <89534894+penumalays@users.noreply.github.com>
2021-08-25 11:28:40 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,19 @@
+/************************************
+*Java Program Name : Hello Java
+*************************************
+*Yuktha Penumala
+*August 24, 2021
+*CMSC 255 C90
+*************************************/
+
+public class Hello {
+	public static void main(String[] args) {
+
+		System.out.println("Yuktha Penumala");
+		System.out.println("Hello Java");
+		System.out.println("Java/Lab 01");
+		System.out.println("August 24, 2021");
+		System.out.println("CMSC 255 C90");
+		System.out.println();
+	}
+}
